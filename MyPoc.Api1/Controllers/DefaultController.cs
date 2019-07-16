c09f8982db362b749b9c219c5fa0ea9f39ce1bbc
Ravindra Saxena <ravindra_s@HCL.COM>
2019-07-17 00:28:12 +0530

    Add project files.

---
+++ MyPoc.Api1/Controllers/DefaultController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyPoc.Api1.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() =>
+             Ok("This is MyPoc-API 1");
+        
+    }
+}
